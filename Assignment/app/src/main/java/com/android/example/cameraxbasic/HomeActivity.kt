d13fab7e4b82986e5a761512b6aff3cb5a0f4457
Varun Wani <varunwani22@gmail.com>
2021-08-02 08:06:43 +0530

    Kagaz

---
+++ Assignment/app/src/main/java/com/android/example/cameraxbasic/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.android.example.cameraxbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
